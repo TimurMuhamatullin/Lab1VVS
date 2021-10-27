565ed5b34b6dfc48dc0bcf74103f2e06aba83aa5
TimurMuhamatullin <79625258307tm@gmail.com>
2021-10-27 11:02:02 +0500

    second

--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -12,9 +12,30 @@ namespace Lab1
 {
     public partial class Form1 : Form
     {
+        double[] epsArray;
+        const int k = 4;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private double f(ref double[] epsAr, int n) {
+            if (n != 0) {
+                double local = Math.Pow(10, -2 * k) * (Convert.ToInt64(Math.Pow(10, 3 * k)* Math.Pow(f(ref epsAr, n - 1), 2)));
+                epsAr[n] = local - (int) local;
+            }
+            return epsAr[n];
+        } 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = 10;
+            epsArray = new double[n];
+            epsArray[0] = 0.73672994;
+            f(ref epsArray, n-1);
+            for (int i = 0; i < n; i++) {
+                label1.Text = label1.Text + epsArray[i].ToString() + "\n";
+            }
         }
     }
 }
