7425a20d7ead73382c602fe55a524bf09d86bdd9
aredo1 <93238899+aredo1@users.noreply.github.com>
2021-11-24 01:07:03 +0500

    testnezavis

--- Lab1/Lab1/Form1.Designer.cs
+++ Lab1/Lab1/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -103,11 +106,41 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Введите восьмизначное число";
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(604, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 60);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "V_V";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Тест независимости";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(654, 240);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(268, 20);
+            this.textBox4.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -131,6 +164,9 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -38,5 +38,29 @@ namespace Lab1
                 textBox3.Text += epsArray[i].ToString() + "\r" + "\n";          
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sum1 = 0, sum2 = 0, sum3 = 0, z, ro_kol, ro_max, p;
+            int n = 10;
+            z = 1.64;
+            for (int i = 0; i < n; i++)
+            {
+                sum1 = sum1 + i * epsArray[i];
+                sum2 = sum2 + (epsArray[i] * ((n + 1) / 2));
+                sum3 = sum3 + epsArray[i];
+            }
+            p = (n ^ 2 - 1) / 12;
+            ro_kol = (((1 / n) * sum1) - ((1 / n) * sum2)) / Math.Sqrt((((1 / n) * sum3) - (Math.Pow(((1 / n) * sum3), 2)) * p));
+            ro_max = z * ((1 - Math.Pow(ro_kol, 2)) / Math.Sqrt(n));
+            if (ro_kol > ro_max)
+            {
+                textBox4.Text = "Существует корреляционная зависимость";
+            }
+            else
+            {
+                textBox4.Text = "Корреляционная зависимость не прослеживается";
+            }
+        }
     }
 }
